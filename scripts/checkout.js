66ad23b3f2d7a39fd1504d34b31f18f251c001cd
maxhtx <max.helman@yahoo.com>
2025-03-03 12:13:39 -0600

    Remove from Cart via productId in class

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -21,7 +21,7 @@ cart.forEach(cartItem => {
     });
     
     cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -109,4 +109,7 @@ document.querySelectorAll(".js-delete-link")
             const productId = link.dataset.productId
             removeFromCart(productId);
 
-    })});
+            document.querySelector(`.js-cart-item-container-${productId}`)
+                .remove();
+        });
+    });
